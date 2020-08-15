a2ae47766e3ab88878aefa94640d429b257de64b
Gilbert Antonius <gantonius@berkeley.edu>
2020-08-15 16:56:01 -0400

    try web app deployment

--- flasky/virus.py
+++ flasky/virus.py
@@ -4,6 +4,7 @@ from pprint import pprint
 from flask_wtf import FlaskForm 
 from wtforms import StringField, SubmitField 
 from wtforms.validators import DataRequired
+from . import app
 
 app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
